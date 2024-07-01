437d4d1e233e51c6a046871686737b6649dcb6a5
Mikey191 <desertigorb@gmail.com>
2024-07-01 16:01:36 +0300

    1-11

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
-import time
+# import time
+#
+# for i in range(10):
+#     print(f"Прогресс: {i}/10", end="\r")
+#     time.sleep(1)
 
-for i in range(10):
-    print(f"Прогресс: {i}/10", end="\r")
-    time.sleep(1)
+
+# a = "hello"
+# b = a[3]
+# c = a[len(a) - 1]
+# print(b)
+# print(c)
+
+# str1 = "Hello my friend!"
+# str2 = str1[0:6]
+# str3 = str1[:6]
+# str4 = str1[6:]
+# str6 = str1[::2]
+# str7 = str1[::-1]
+# str8 = str1[4::-1]
+# print(str2)
+# print(str3)
+# print(str4)
+# print(str6)
+# print(str7)
+# print(str8)
+#
+# str1 = "Hello World!"
+# str1 = "K" + str1[1:]
+
+# main_str = input()
+# first_char = main_str[0]
+# last_char = main_str[-1]
+# print(first_char, last_char)
+
+main_str = "Hello"
+print(id(main_str))
+print(id(main_str[0]))
+
+
