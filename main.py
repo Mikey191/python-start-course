6693a64d86815944a922f28a3ccd8d6d44dc917a
Mikey191 <desertigorb@gmail.com>
2024-08-01 16:05:04 +0300

    Вторая самостоятельная

--- main.py
+++ main.py
@@ -452,6 +452,392 @@
 #     count += 1
 
 # Создание списка с парами чисел от 1 до 3
-pairs = [(x, y, z) for x in range(1, 4) for y in range(1, 4) for z in range(1,4)]
+# pairs = [(x, y, z) for x in range(1, 4) for y in range(1, 4) for z in range(1,4)]
 # Результат: [(1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3)]
-print(pairs)
+# print(pairs)
+#
+# lst_m_31 = [1, 3, 5, 7, 8, 10, 12]
+# lst_m_30 = [4, 6, 9, 11]
+# lst_m_28 = [2]
+# m = 3
+# n = 30
+# if m in lst_m_31:
+#     if n == 31:
+#         print(m, n - 1)
+#         print(m+1, end=" ")
+#         print(1)
+#     else:
+#         print(m, n - 1)
+#         print(m, n + 1)
+# elif m in lst_m_30:
+#     print()
+# else:
+#     print()
+
+# str_in = "dfраdedрафысфсцусцура"
+# sub_str = "ра"
+# for index in range(len(str_in)):
+#     if sub_str in str_in[index:index+len(sub_str)]:
+#         print(index)
+
+# lst = [1, 2, 3, 4, 5, 6]
+# str = "ejhcvjsevhc"
+#
+# g_phone_number = "+7(928)714-49-14"
+# sub_str = "5"
+# flag = False
+# for index, char in enumerate(g_phone_number):
+#     print(index)
+#     print(char)
+#
+# for char in g_phone_number:
+#     print(char)
+#
+# for index in range(len(g_phone_number)):
+#     print(index)
+#     print(g_phone_number[index])
+#
+#
+# numbers = [1, 2, 3, 4, 5, 6]
+# iterator = iter(numbers)
+#
+# print(next(iterator))  # Выводит 1
+# print(next(iterator))  # Выводит 2
+# print(next(iterator))  # Выводит 3
+
+
+#
+# r = range(5)
+# iterator = iter(r)
+#
+# for _ in range(3):
+#     print(next(iterator))
+#
+# print()
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+
+# lst = ['one', 'two', 'three', 'four', 'five']
+# r_lst = []
+# for index in range(len(lst)):
+#     r_lst.append(lst[index])
+#     r_lst.append(lst[index])
+#
+# print(r_lst)
+
+# Самостоятельная
+# users = [
+#     ["Александр", "2aB3c", "user"],
+#     ["Михаил", "5Xy1Z", "user"],
+#     ["Иван", "9mN7p", "admin"],
+#     ["Дмитрий", "4Qw6R", "user"],
+#     ["Андрей", "3sT9u", "admin"]
+# ]
+#
+# questions = ["Сколько вам лет? ", "Чем любите заниматься? ", "Ваш любимый фильм? "]
+# # дополнительные списки
+# lst_man = []
+# user_lst = []
+#
+# # использование цикла while
+# while True:
+#     print("\nВы входите в программу заного!\n")
+#     login = input("Введите логин: ")
+#     # использование условного оператора
+#     if login.lower() == "выход":
+#         print("Досвидания!")
+#         break
+#     password = input("Введите пароль: ")
+#     # использование булевого флага
+#     flag = False
+#     # использование цикла for
+#     for user in users:
+#         if login == user[0] and password == user[1]:
+#             if user[2] == "admin":
+#                 flag = True
+#                 print("Вы вошли как админ!")
+#                 user_lst.append(input("Придумайте логин для нового пользователя: "))
+#                 user_lst.append(input("Придумайте парольдля нового пользователя: "))
+#                 user_lst.append(input("Укажите его роль для нового пользователя: "))
+#                 print(f"Вы создали пользователя {user_lst}")
+#             else:
+#                 flag = True
+#                 lst_man.append(login)
+#                 for index in range(len(questions)):
+#                     lst_man.append(input(questions[index]))
+#     if not flag:
+#         print("Неверный логин или пароль, попробуйте заного")
+#         continue
+#     if len(lst_man):
+#         print(f"\nПривет, меня зовут {lst_man[0]}.")
+#         print(f"Мне {lst_man[1]} лет, я люблю {lst_man[2]}.")
+#         print(f"Мой любимый фильм - {lst_man[3]}.\n")
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Напишите программу, которая будет запрашивать у пользователя количество студентов, их имена, фамилии и оценки по трем предметам.
+# Программа должна вычислять среднюю оценку.
+# Программа должна добавлять строку "низкая"/"средняя"/"высокая" в результирующий список с информацией студента исходя из средней оценки.
+# Если меньше 8 - "низкая", от 8 до 10 - "средняя", от 10 и выше - "высокая".
+# Для каждого студента должен формироваться список типа: [Имя студента, первая оценка, вторая оценка, третья оценка, средняя оценка, низкая/средняя/высокая]
+# Программа должна выводить список всех студентов с их информацией.
+# Пример вывода:
+# Информация о студентах:
+# 1. Ivan Petrov: Оценки: [8, 7, 9], Средняя оценка: 8.00 (средняя)
+# 2. Sergey Ivanov: Оценки: [6, 5, 8], Средняя оценка: 6.33 (низкая)
+# Так же программа должна вывести всех студентов с низкой оценкой.
+# Пример вывода:
+# Информация о студентах с низкой оценкой:
+# 1. Sergey Ivanov - 6.33
+
+# # Запрашиваем количество студентов
+# students_count = int(input())
+# # Результирующий список
+# students = []
+# # Счетчик для выхода из цикла
+# count = 0
+# # Цикл, который будет работать пока не введем всех студентов
+# while count < students_count:
+#     # Запрашиваем имя
+#     name_student = input("name: ")
+#
+#     # Запрашиваем оценки по трем предметам
+#     scores = [int(input(f"{i+1} score: ")) for i in range(3)]
+#
+#     # Средняя оценка студента
+#     avg_score = sum(scores)/len(scores)
+#
+#     # Строка "низкая"/"средняя"/"высокая"
+#     str_score = "низкая" if avg_score < 8 else "средняя" if avg_score < 11 else "высокая"
+#
+#     # Нужно помнить, что списки мы можем складывать только со списками
+#     students.append([name_student] + scores + [avg_score] + [str_score])
+#
+#     count += 1
+#
+# print(students)
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+# n = 14
+# for number in range(2, n):
+#     if n % number == 0:
+#         print("Число не простое")
+#         break
+#     else:
+#         continue
+# else:
+#     print("Число простое")
+
+# str = input()
+# iterator = iter(str)
+# temp = None
+# while temp != " ":
+#     temp = next(iterator)
+#     print(temp)
+
+# count = 1
+# while count <= 3:
+#     for i in range(1, 4):
+#         print(i, end=' ')
+#     count += 1
+
+# for i in range(1, 4):
+#     count = 1
+#     while count <= 3:
+#         print(i, end=' ')
+#         count += 1
+
+# for i in range(1, 4):
+#     for j in range(0, 3):
+#         print(i, end=" ")
+#         print(j)
+#     print()
+
+# lst = [
+#     [1, 2, 3, 4],
+#     [2, 3, 4, 5],
+#     [3, 4, 5, 6]
+# ]
+#
+# for row in lst:
+#     for number in row:
+#         print(number, end=" ")
+#
+# lst1 = [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]]
+# lst2 = [[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]]
+#
+# r_lst = []
+# for i in range(len(lst1)):
+#     sublist = []
+#     for j in range(len(lst2[i])):
+#         print(f"{lst1[i][j]} + {lst2[i][j]}")
+#         sublist.append(lst1[i][j] + lst2[i][j])
+#     r_lst.append(sublist)
+# print(r_lst)
+
+# lst = [ "Я   помню  чудное    мгновенье",
+# "Передо  мной    явилась   ты",
+# "Как    мимолетное   виденье",
+# "Как     гений   чистой  красоты"]
+#
+# for index, line in enumerate(lst):
+#     while lst[index].count("  "):
+#         lst[index] = lst[index].replace("  ", " ")
+#
+# print(lst)
+
+# M = int(input("Введите количество строк: "))
+# N = int(input("Введите количество столбцов: "))
+#
+# nested_list = []
+# for i in range(M):
+#     sublist = []
+#     for j in range(N):
+#         sublist.append(0)
+#     nested_list.append(sublist)
+#     print(nested_list[i])
+
+
+# for i in range(M):
+#     for j in range(N):
+#         print(nested_list[i][j], end=' ')
+#     print()
+
+# for i in range(M):
+#     for j in range(N):
+#         nested_list[i][j] = 1
+#         print(nested_list[i][j], end=" ")
+#     print()
+
+# for i in range(M):
+#     for j in range(N):
+#         print(nested_list[i][j], end=' ')
+#     print()
+
+# lst = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+# for i in range(len(lst)):
+#     for j in range(len(lst[i])):
+#         print(lst[i][j], end='\t')
+#     print()
+#
+# print()
+#
+# for i in range(len(lst)):
+#     for j in range(i + 1, len(lst)):
+#         lst[i][j], lst[j][i] = lst[j][i], lst[i][j]
+#
+# for row in lst:
+#     for number in row:
+#         print(number, end='\t')
+#     print()
+
+# lst = [[1,2,3,4], [5,6,7,8], [9,10,11,12]]
+
+
+# task = "12-11+30"
+# task = task.replace(" ", "")
+# result = ""
+# for index in range(len(task)):
+#     if task[index] == "+":
+#         result = task[:index] + " " + task[index] + " " + task[index + 1:]
+#     if task[index] == "-":
+#         result = task[:index] + " " + task[index] + " " + task[index + 1:]
+#
+# print(result)
+
+# numbers = [0 for number in range(1, 11)]
+# print(numbers)
+#
+# squares = [i**2 for i in range(1, 11)]
+# print(squares)
+
+# Ввод чисел с клавиатуры и создание списка
+# numbers = [int(i) for i in input().split()]
+# print(numbers)
+
+# # Список городов
+# cities = ['Москва', 'Санкт-Петербург', 'Нью-Йорк', 'Лондон', 'Париж', 'Мадрид']
+# # Создание списка городов, начинающихся с буквы "М"
+# filtered_cities = [city for city in cities if city.startswith('М')]
+# print(filtered_cities)
+
+# Список чисел
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# # Создание списка четных чисел
+# even_numbers = [num for num in numbers if num % 2 == 0]
+# print(even_numbers)
+
+# Создание списка с парами чисел от 1 до 3
+# pairs = [(x, y) for x in range(1, 4) for y in range(1, 4)]
+# pairs = []
+# for x in range(1, 4):
+#     for y in range(1, 4):
+#         pairs.append((x, y))
+# # Результат: [(1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3)]
+# print(pairs)
+
+# # Преобразование двумерного списка в одномерный
+# matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# flat_list = [num for row in matrix for num in row]
+# # Результат: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(flat_list)
+#
+
+# # Получение двумерного списка с помощью вложенного генератора списков
+# matrix = [[i+j for j in range(3)] for i in range(3)]
+# # Результат: [[0, 1, 2], [1, 2, 3], [2, 3, 4]]
+# print(matrix)
+
+# Возвести все значения двумерного списка в квадрат с помощью вложенного генератора списков
+# matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# squared_matrix = [ [num**2 for num in row] for row in matrix]
+# # Результат: [[1, 4, 9], [16, 25, 36], [49, 64, 81]]
+# print(squared_matrix)
+
+# # Транспонирование матрицы с использованием вложенных генераторов
+# matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# transposed_matrix = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+# # Результат: [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+# print(transposed_matrix)
+
+# # Пример: Создание списка кубов чисел от 1 до 5 с использованием вложенного генератора списков
+# cubes = [num ** 2 for num in [value + 1 for value in range(5)]]
+# # Результат: [1, 8, 27, 64, 125]
+# print(cubes)
+
+# task = "12 + 13-5"
+# task = task.replace("-", " - ")
+# task = task.replace("+", " + ")
+# # while "  " in task:
+# #     task = task.replace("  ", " ")
+# task = task.replace("  ", " ")
+# print(task)
+
+# task = "12 + 13 -5"
+# i = 0
+# # task = task[:i] + task[i+1:]
+# while " " in task:
+#     if " " in task[i]:
+#         task = task[:i] + task[i+1:]
+#     i += 1
+#
+# print(task)
+
+# # Входные данные:
+# scores_students = [[10, 11, 11], [11, 9, 12], [8, 12, 12]]
+# names_students = [["Александр","Александров"], ["Михаил","Иванов"], ["Дмитрий","Петров"]]
+#
+# # Объединение в один список
+# # [['Александр', 'Александров', 10, 11, 11], ['Михаил', 'Иванов', 11, 9, 12], ['Дмитрий', 'Петров', 8, 12, 12]] с помощью генератора списков.
+# students = [names_students[i] + scores_students[i] for i in range(len(names_students))]
+# print(students)
+#
+# # Добавление к каждому списку средней оценки с помощью обычного цикла. Округлив до одного знака после запятой.
+# for index in range(len(students)):
+#     students[index].append(round(sum(students[index][2:]) / len(students[index][2:]), 1))
+# print(students)
+
--- main2.py
+++
@@ -1,6 +0,0 @@
-a = float(input())
-b = int(a)
-print(b)
-# a = input()
-# a = int(a)
-
